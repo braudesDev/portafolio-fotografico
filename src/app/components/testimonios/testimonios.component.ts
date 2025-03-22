40ba50bf999942bb33af42fba7d7cd64b47fa43c
braudesDev <braudesdev@gmail.com>
2025-03-22 01:22:20 -0600

    Seguimos puliendo el apartado de testimonios, se agrego un tooltip que ayudara al usuario a saber como obtener una url valida para publicar un testimonio con su foto de perfil

--- src/app/components/testimonios/testimonios.component.ts
+++ src/app/components/testimonios/testimonios.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importa CommonModule aquí
 import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
 import { TestimoniosService } from '../../servicios/testimonios.service';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-testimonios',
   standalone: true, // Indica que el componente es standalone
-  imports: [FormsModule, CommonModule], // Añades FormsModule aquí
+  imports: [FormsModule, CommonModule, MatIcon, MatTooltipModule], // Añades FormsModule aquí
   templateUrl: './testimonios.component.html',
   styleUrls: ['./testimonios.component.css']
 })
@@ -14,6 +16,11 @@ export class TestimoniosComponent implements OnInit {
   testimonios: any[] = [];
   nuevoTestimonio = { nombre: '', comentario: '', imagen_url: '' };
   mensaje = '';
+  instruccionesTooltip = `
+    1. Sube tu foto a un servicio como Imgur o Postimages.
+    2. Copia la URL de la imagen (debe terminar en .jpg, .png, etc.).
+    3. Pega la URL en el campo "Foto (URL opcional)".
+  `;
 
   constructor(private testimoniosService: TestimoniosService) {}
 
