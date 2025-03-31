1d574628cfcd3ec36ec72d64edaec3f482160401
braudesDev <braudesdev@gmail.com>
2025-03-31 03:45:35 -0600

    Se cambiaron los enviroments y tsconfig.json

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  production: false,
   firebaseConfig: {
-    apiKey: process.env["REACT_APP_FIREBASE_API_KEY"] as string,
-    authDomain: process.env["REACT_APP_FIREBASE_AUTH_DOMAIN"] as string,
-    projectId: process.env["REACT_APP_FIREBASE_PROJECT_ID"] as string,
-    storageBucket: process.env["REACT_APP_FIREBASE_STORAGE_BUCKET"] as string,
-    messagingSenderId: process.env["REACT_APP_FIREBASE_MESSAGING_SENDER_ID"] as string,
-    appId: process.env["REACT_APP_FIREBASE_APP_ID"] as string,
-    measurementId: process.env["REACT_APP_FIREBASE_MEASUREMENT_ID"] as string
+    apiKey: "AIzaSyC0I5F48FsDI-02eS_U7Nt6SYsWVPQ3_Jc",
+    authDomain: "portafolio-fotografico-15a5b.firebaseapp.com",
+    projectId: "portafolio-fotografico-15a5b",
+    storageBucket: "portafolio-fotografico-15a5b.firebasestorage.app",
+    messagingSenderId: "28833430981",
+    appId: "1:28833430981:web:5bd9d0e0cf36777d9a5fac",
+    measurementId: "G-3SEDBH37SR"
   }
 };
