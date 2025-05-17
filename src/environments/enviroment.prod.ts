aa598156655da46506e5f024398dbcdad4e9edf9
braudesDev <braudesdev@gmail.com>
2025-05-17 16:49:11 -0600

    fix: rename enviroments to environments

---
+++ src/environments/enviroment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "REDACTED",
+    authDomain: "portafolio-fotografico-15a5b.firebaseapp.com",
+    projectId: "portafolio-fotografico-15a5b",
+    storageBucket: "portafolio-fotografico-15a5b.firebasestorage.app",
+    messagingSenderId: "28833430981",
+    appId: "1:28833430981:web:5bd9d0e0cf36777d9a5fac",
+    measurementId: "G-3SEDBH37SR"
+  }
+};
