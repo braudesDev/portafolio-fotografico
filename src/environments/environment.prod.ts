26dbb89aa9016b04c024cbd2a5f3557e26b5c658
braudesDev <braudesdev@gmail.com>
2026-01-30 22:50:31 -0600

    Se agrego el google aanlitycs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    authDomain: "portafolio-fotografico-15a5b.firebaseapp.com",
-    projectId: "portafolio-fotografico-15a5b",
-    storageBucket: "portafolio-fotografico-15a5b.firebasestorage.app",
-    messagingSenderId: "28833430981",
-    appId: "1:28833430981:web:5bd9d0e0cf36777d9a5fac",
-    measurementId: "G-3SEDBH37SR"
+    authDomain: 'portafolio-fotografico-15a5b.firebaseapp.com',
+    projectId: 'portafolio-fotografico-15a5b',
+    storageBucket: 'portafolio-fotografico-15a5b.firebasestorage.app',
+    messagingSenderId: '28833430981',
+    appId: '1:28833430981:web:5bd9d0e0cf36777d9a5fac',
+    measurementId: 'G-3SEDBH37SR',
   },
-  baseUrl: 'https://onoff.shot.com',
-  imageBaseUrl: 'https://onoffshot.com/assets/images'
+  baseUrl: 'https://onoffshot.com',
+  imageBaseUrl: 'https://onoffshot.com/assets/images',
 };
